30f209f0727ce6662c120ed42637a6c4761c2cbe
Fickle <6804983@qq.com>
2022-06-26 12:37:27 +0800

    first commit

---
+++ Test-1/src/main/java/aaa.java
@@ -0,0 +1,65 @@
+import javax.xml.crypto.Data;
+import java.util.HashMap;
+
+/**
+ * @author fickle
+ * @create 2022-06-15 14:45
+ */
+public class aaa {
+    public static void main(String[] args) {
+        System.out.println(111);
+        System.out.println(Math.round(-1.6));
+        HashMap<Object, Object> map = new HashMap<>();
+
+
+
+    }
+
+    //冒泡排序
+    public static void bubbleSort(int[] arr) {
+        int len = arr.length;
+        for (int i = 0; i < len; i++) {
+            for (int j = 0; j < len - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+
+
+    // 归并排序
+    public static void mergeSort(int[] arr, int left, int right) {
+        if (left < right) {
+            int mid = (left + right) / 2;
+            mergeSort(arr, left, mid);
+            mergeSort(arr, mid + 1, right);
+            merge(arr, left, mid, right);
+        }
+    }
+    // merge
+    public static void merge(int[] arr, int left, int mid, int right) {
+        int[] temp = new int[right - left + 1];
+        int i = left, j = mid + 1, k = 0;
+        while (i <= mid && j <= right) {
+            if (arr[i] < arr[j]) {
+                temp[k++] = arr[i++];
+            } else {
+                temp[k++] = arr[j++];
+            }
+        }
+        while (i <= mid) {
+            temp[k++] = arr[i++];
+        }
+        while (j <= right) {
+            temp[k++] = arr[j++];
+        }
+        for (int m = 0; m < temp.length; m++) {
+            arr[left + m] = temp[m];
+        }
+    }
+
+}
